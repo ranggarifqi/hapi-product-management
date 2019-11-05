a908cf06c00516d2c9ab7fea3d0985a911fe8100
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-11-05 19:44:30 +0700

    Setup Knex Connection for PG

--- server/manifest.js
+++ server/manifest.js
@@ -1,65 +1,67 @@
-'use strict';
+"use strict";
 
-const Dotenv = require('dotenv');
-const Confidence = require('confidence');
-const Toys = require('toys');
+const Dotenv = require("dotenv");
+const Confidence = require("confidence");
+const Toys = require("toys");
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
 
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
-    server: {
-        host: 'localhost',
-        port: {
-            $env: 'PORT',
-            $coerce: 'number',
-            $default: 3000
-        },
-        debug: {
-            $filter: { $env: 'NODE_ENV' },
-            $default: {
-                log: ['error'],
-                request: ['error']
-            },
-            production: {
-                request: ['implementation']
-            }
-        }
+  server: {
+    host: "localhost",
+    port: {
+      $env: "PORT",
+      $coerce: "number",
+      $default: 3000
     },
-    register: {
-        plugins: [
-            {
-                plugin: '../lib', // Main plugin
-                options: {}
-            },
-            {
-                plugin: 'schwifty',
-                options: {
-                    $filter: 'NODE_ENV',
-                    $default: {},
-                    $base: {
-                        migrateOnStart: true,
-                        knex: {
-                            client: 'sqlite3',
-                            useNullAsDefault: true,     // Suggested for sqlite3
-                            connection: {
-                                filename: ':memory:'
-                            }
-                        }
-                    },
-                    production: {
-                        migrateOnStart: false
-                    }
-                }
-            },
-            {
-                plugin: {
-                    $filter: { $env: 'NODE_ENV' },
-                    $default: 'hpal-debug',
-                    production: Toys.noop
-                }
-            }
-        ]
+    debug: {
+      $filter: { $env: "NODE_ENV" },
+      $default: {
+        log: ["error"],
+        request: ["error"]
+      },
+      production: {
+        request: ["implementation"]
+      }
     }
+  },
+  register: {
+    plugins: [
+      {
+        plugin: "../lib", // Main plugin
+        options: {}
+      },
+      {
+        plugin: "schwifty",
+        options: {
+          $filter: "NODE_ENV",
+          $default: {},
+          $base: {
+            migrateOnStart: true,
+            knex: {
+              client: "pg",
+              connection: {
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE
+              }
+            }
+          },
+          production: {
+            migrateOnStart: false
+          }
+        }
+      },
+      {
+        plugin: {
+          $filter: { $env: "NODE_ENV" },
+          $default: "hpal-debug",
+          production: Toys.noop
+        }
+      }
+    ]
+  }
 });
